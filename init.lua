89c62dea25573ec3e4214015f39e156c210a7eef
Harvey Lewis <harvey.l@hotmail.co.uk>
2024-05-13 10:44:29 +0100

    add wakatime

--- init.lua
+++ init.lua
@@ -912,6 +912,7 @@ require('lazy').setup({
       }
     end,
   },
+  { 'wakatime/vim-wakatime', lazy = false },
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
